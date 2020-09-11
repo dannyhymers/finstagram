8d254944a2860faf36f4b6dce6796c2143164157
dannyhymers <daniel-7750@hotmail.com>
2020-09-11 03:27:13 +0000

    new

--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,24 @@
-get '/' do
-  @finstagram_posts = FinstagramPost.order(created_at: :desc)
-  erb(:index)
+post '/signup' do
+
+ 
+  email      = params[:email]
+  avatar_url = params[:avatar_url]
+  username   = params[:username]
+  password   = params[:password]
+
+  user = User.new({email: email, avatar_url: avatar_url, username: username, password: password })
+
+
+  if user.save
+
+
+    escape_html user.inspect
+
+
+  else
+    
+    
+    escape_html user.errors.full_messages
+ 
+  end
 end
